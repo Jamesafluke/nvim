39088bc99e184f842ece27bb7ef32f4f9dfd449d
James Fluckiger <v-jfluckiger@microsoft.com>
2025-01-18 16:24:43 -0700

    Add :te and copy paste magic

--- init.lua
+++ init.lua
@@ -5,3 +5,6 @@ vim.opt.tabstop = 4        -- Number of spaces a tab character represents
 vim.opt.shiftwidth = 4     -- Number of spaces to use for each level of indentation
 vim.opt.expandtab = true   -- Convert tabs to spaces
 vim.opt.softtabstop = 4    -- Number of spaces inserted when pressing <Tab>
+vim.opt.clipboard = "unnamedplus" -- Makes y or d etc also use the system clipboard
+
+vim.api.nvim_create_user_command("Te", "terminal", {})
