9bf7e182971188f043e83dd2df36faf8d2f5da1d
James Fluckiger <v-jfluckiger@microsoft.com>
2025-01-18 16:49:58 -0700

    Add some things

--- init.lua
+++ init.lua
@@ -6,5 +6,7 @@ vim.opt.shiftwidth = 4     -- Number of spaces to use for each level of indentat
 vim.opt.expandtab = true   -- Convert tabs to spaces
 vim.opt.softtabstop = 4    -- Number of spaces inserted when pressing <Tab>
 vim.opt.clipboard = "unnamedplus" -- Makes y or d etc also use the system clipboard
+vim.wo.relativenumber = true
 
 vim.api.nvim_create_user_command("Te", "terminal", {})
+
