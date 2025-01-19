e69721c1c887c01a0f7ff09b417cd16e79e5a481
James Fluckiger <v-jfluckiger@microsoft.com>
2025-01-19 07:22:24 -0700

    Stuff

--- init.lua
+++ init.lua
@@ -8,6 +8,6 @@ vim.opt.softtabstop = 4    -- Number of spaces inserted when pressing <Tab>
 vim.opt.clipboard = "unnamedplus" -- Makes y or d etc also use the system clipboard
 vim.wo.relativenumber = true
 
-vim.api.nvim_create_user_command("Te", "terminal", {})
+vim.api.nvim_create_user_command("Te", "ToggleTerm", {})
 vim.api.nvim_create_user_command("Tree", "NvimTreeOpen", {})
 
