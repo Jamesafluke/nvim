84ae5e58975cdd35711c3bce41c2ff9ceeebd29f
jamesafluke <jamesafluke@gmail.com>
2025-01-21 12:51:01 -0700

    Some more changes

--- init.lua
+++ init.lua
@@ -4,18 +4,19 @@
 require("config.lazy")
 --require("config.lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
 --require("plugins")
-require 'nvim-treesitter.install'.prefer_git = false
 
 vim.opt.tabstop = 4        -- Number of spaces a tab character represents
 vim.opt.shiftwidth = 4     -- Number of spaces to use for each level of indentation
 vim.opt.expandtab = true   -- Convert tabs to spaces
 vim.opt.softtabstop = 4    -- Number of spaces inserted when pressing <Tab>
 vim.opt.clipboard = "unnamedplus" -- Makes y or d etc also use the system clipboard
+vim.opt.wrap = false
 vim.wo.number = true
 vim.wo.relativenumber = true
 
 vim.api.nvim_create_user_command("Te", "ToggleTerm", {})
 vim.api.nvim_create_user_command("Tree", "NvimTreeOpen", {})
+vim.api.nvim_create_user_command("Tele", "Telescope", {})
 
 --For Kinesis Keyboard
 local opts = { noremap = true, silent = true }
@@ -37,3 +38,10 @@ vim.api.nvim_set_keymap('v', 'i', '<Right>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', 'l', 'i', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', 'm', 'n', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', 'L', 'I', { noremap = true, silent = true })
+
+-- For navigating split view:
+-- However, this doesn't work at all. For now I'll try using Ctrl+w+w to navigate.
+-- https://neovim.io/doc/user/usr_08.html
+vim.api.nvim_set_keymap('n', '<C-w>n', '<C-w><j>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w>e', '<C-w><k>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w>i', '<C-w><l>', { noremap = true, silent = true })
