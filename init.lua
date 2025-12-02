4f6958168104d5cf05f6bdb696100072ac239bc5
Jamesafluke <42627144+Jamesafluke@users.noreply.github.com>
2025-12-02 10:30:44 -0700

    Indentation settings
    Added key mappings for closing tabs and indenting lines in visual mode.

--- init.lua
+++ init.lua
@@ -1,8 +1,14 @@
-local kinesis = true
+#ctl w for closing tabs
+vim.keymap.set("n", "<C-w>", "<Nop>", { silent = true })
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.undofile = true
+
+--Indent selected lines but stay selected.
+local opts = { noremap = true, silent = true }
+vim.keymap.set('v', '>', '>gv', opts)
+vim.keymap.set('v', '<', '<gv', opts)
 
 vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
